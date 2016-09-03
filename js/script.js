1a865ba39d5a2fcef30f04267b8114fccd3a36f9
“kasper341” <kasper_341@hotmail.com>
2016-09-03 15:26:37 -0700

    Add second cat

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    var click_count = 0;
+
+    var cat_object = {
+        cat1: {
+            name: "cat1"
+        },
+        cat2: {
+            name: "cat2"
+        }
+    };
+
+
+    $("#cat").click(function(event){
+        console.log("click", event);
+        // event.preventDefault();
+        console.log("clicked", click_count++);
+    });
+
+    $("#cat1").append("<p>"+cat_object.cat1.name+"</p>")
+    $("#cat2").append("<p>"+cat_object.cat2.name+"</p>")
+
+    // $("#cat1").click();
 });
