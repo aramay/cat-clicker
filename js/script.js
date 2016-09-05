582f82bb3f56283921b52bc6e3d41bfd0793b309
“kasper341” <kasper_341@hotmail.com>
2016-09-05 11:30:34 -0700

    add function to count clicks

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    var click_count = 0;
+    // var click_count = 0;
+    function catClicked(event){
+        // for (var i = 1; i < 3; i ++) {
+        //     num = i;
+        //     $("#cat"+i).append("<p>"+cat_object[i].name+"</p>")
+        //     // $("#cat2").append("<p>"+cat_object[prop].name+"</p>")
+        //     $("#cat").each(function(){
+        //         console.log("click", event);
+        //
+        //         $(this).click(function(){
+        //             console.log("clicked");
+        //         }) ;
+        //
+        //     }) ;
+        // }
+        console.log(event);
+
+    }
 
     var cat_object = {
-        cat1: {
-            name: "cat1"
+        "1": {
+            name: "cat1",
+            click_count: 0
         },
-        cat2: {
-            name: "cat2"
+        "2": {
+            name: "cat2",
+            click_count: 0
         }
     };
 
 
-    $("#cat").click(function(event){
-        console.log("click", event);
-        // event.preventDefault();
-        console.log("clicked", click_count++);
-    });
 
-    $("#cat1").append("<p>"+cat_object.cat1.name+"</p>")
-    $("#cat2").append("<p>"+cat_object.cat2.name+"</p>")
 
-    // $("#cat1").click();
+    $("#cat-list").on("click", catClicked);
 });
